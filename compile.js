f6f9af0b251f75cd683ba52b1a2c5aeb6320dcae
Ulguim <williamulguim2014@gmail.com>
2023-05-28 03:57:35 -0300

    :building_construction: update deploy

--- compile.js
+++ compile.js
@@ -10,15 +10,15 @@ gulpProcess.stderr.on('data', function(data) {
   console.error('Gulp error: ' + data.toString());
 });
 
-const timeout = 60000; // Tempo limite em milissegundos (60 segundos)
+const timeout = 20000; // Tempo limite em milissegundos (60 segundos)
 
 const timer = setTimeout(function() {
   console.log('Tempo limite excedido. Encerrando o processo Gulp.');
-  gulpProcess.kill('SIGINT'); // Encerra o processo Gulp enviando um sinal SIGINT
+  process.kill(process.pid, 'SIGKILL'); // Encerra o processo Gulp enviando um sinal SIGINT
 }, timeout);
 
-gulpProcess.on('exit', function(code) {
-  clearTimeout(timer); // Limpa o temporizador
-  console.log('Gulp process exited with code ' + code);
-  process.exit(); // Encerra o programa após a conclusão do Gulp
-});
+// gulpProcess.on('exit', function(code) {
+//   clearTimeout(timer); // Limpa o temporizador
+//   console.log('Gulp process exited with code ' + code);
+//   process.kill(process.pid, 'SIGKILL');
+// });
